07f8c441cfec4ebbb9ef9c9acbd368f043a83141
JuanSoftware <js33neospro@gmail.com>
2023-09-24 15:33:02 -0400

    te amo cSharp
    Agora vou entrar na parte que eu quero: abstracão

---
+++ DesafioAbstracao/DesafioAbstracaoCelular/DesafioAbstracaoCelular/Nokia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioAbstracaoCelular
+{
+    // TODO: Herdar da classe "Smartphone"
+    internal class Nokia : Smartphone
+    {
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+    }
+}
