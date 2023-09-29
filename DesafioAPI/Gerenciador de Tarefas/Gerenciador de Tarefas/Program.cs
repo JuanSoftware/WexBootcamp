80c48df5265e964d7d39c8eafa6572f247eae5a9
JuanSoftware <js33neospro@gmail.com>
2023-09-29 09:01:30 -0400

    desafio

---
+++ DesafioAPI/Gerenciador de Tarefas/Gerenciador de Tarefas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gerenciador_de_Tarefas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
